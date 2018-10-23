298142a7e2d870d4a19dfc37728d6fbad883d832
Loic Bertin <loiberti@z4r3p2.le-101.fr>
2018-10-23 16:17:03 +0200

    updates gnl

--- gnl/get_next_line.c
+++ gnl/get_next_line.c
@@ -5,54 +5,51 @@
 /*                                                 +:+:+   +:    +:  +:+:+    */
 /*   By: loiberti <marvin@le-101.fr>                +:+   +:    +:    +:+     */
 /*                                                 #+#   #+    #+    #+#      */
-/*   Created: 2018/10/10 12:53:38 by loiberti     #+#   ##    ##    #+#       */
-/*   Updated: 2018/10/20 15:08:44 by loiberti    ###    #+. /#+    ###.fr     */
+/*   Created: 2018/10/23 11:42:57 by loiberti     #+#   ##    ##    #+#       */
+/*   Updated: 2018/10/23 11:43:21 by loiberti    ###    #+. /#+    ###.fr     */
 /*                                                         /                  */
 /*                                                        /                   */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
-#include "../libft/libft.h"
-#include <stdio.h>
 
-char	*ft_return_rest(char *str)
+static char			*ft_return_rest(char *str)
 {
 	int		i;
 
-	i = 0;
+	i = -1;
 	if (ft_strchr(str, '\n'))
 	{
-		while (str[i] != '\n')
-		{
-			if (!(str[i]))
-				return (NULL);
-			i++;
-		}
+		while (*str++ != '\n')
+			;
 		if (*str == '\n')
 			return (&str[i + 1]);
 	}
 	return (&str[i + 1]);
 }
 
-char	*ft_get_line(char *to_transform, char *rest)
+static char			*ft_get_line(char *to_transform, char *rest)
 {
 	int		i;
 	char	*tmp;
 
+	i = 0;
 	if (ft_strchr(to_transform, '\n'))
 	{
 		i = ft_strlen(to_transform) - ft_strlen(ft_strchr(to_transform, '\n'));
-		to_transform = ft_strsub(to_transform, 0, i);
+		if (!(to_transform = ft_strsub(to_transform, 0, i)))
+			return (NULL);
 		return (to_transform);
 	}
-	tmp = ft_strjoin(to_transform, rest);
+	if (!(tmp = ft_strjoin(to_transform, rest)))
+		return (NULL);
 	ft_strdel(&rest);
 	rest = tmp;
 	ft_bzero(to_transform, ft_strlen(to_transform));
 	return (rest);
 }
 
-int		ft_read_and_get_rt(const int fd, char **line, char *rest)
+static int			ft_read_and_get_rt(const int fd, char **line, char *rest)
 {
 	int		rt;
 	char	buf[BUFF_SIZE + 1];
@@ -61,14 +58,16 @@ int		ft_read_and_get_rt(const int fd, char **line, char *rest)
 	while ((rt = read(fd, buf, BUFF_SIZE)) > 0)
 	{
 		buf[rt] = '\0';
-		tmp = ft_strjoin(*line, buf);
+		if (!(tmp = ft_strjoin(*line, buf)))
+			return (-1);
 		ft_strdel(line);
 		*line = tmp;
 		if (ft_strchr(*line, '\n'))
 		{
 			if (rest)
 			{
-				tmp = ft_strjoin(rest, *line);
+				if (!(tmp = ft_strjoin(rest, *line)))
+					return (-1);
 				ft_strdel(line);
 				*line = tmp;
 			}
@@ -78,25 +77,27 @@ int		ft_read_and_get_rt(const int fd, char **line, char *rest)
 	return (rt);
 }
 
-int		get_next_line(const int fd, char **line)
+int					get_next_line(const int fd, char **line)
 {
-	static char		*rest[65536];
+	static char		*rest[4864];
 	int				rt;
 
-	if (fd < 0 || line == NULL || !(*line = ft_strnew(0)) || (!(rest[fd]) &&
-	!(rest[fd] = "")))
+	if (fd < 0 || fd > 4864 || !line || !(*line = ft_strnew(0)) ||
+	(!(rest[fd]) && !(rest[fd] = "")) || BUFF_SIZE < 1)
 		return (-1);
 	if ((rt = ft_read_and_get_rt(fd, line, (rest[fd]))) == -1)
 		return (-1);
 	else if (rt > 0)
 	{
 		rest[fd] = ft_return_rest(*line);
-		*line = ft_get_line(*line, rest[fd]);
+		if (!(*line = ft_get_line(*line, rest[fd])))
+			return (-1);
 		return (1);
 	}
 	else if (!rt && ft_strlen(rest[fd]))
 	{
-		*line = ft_get_line(rest[fd], *line);
+		if (!(*line = ft_get_line(rest[fd], *line)))
+			return (-1);
 		rest[fd] = ft_return_rest(rest[fd]);
 		return (1);
 	}
@@ -104,20 +105,3 @@ int		get_next_line(const int fd, char **line)
 		return (1);
 	return (0);
 }
-
-/*int		main(int argc, char **argv)
-{
-	int		fd;
-	char	*line;
-	int i;
-
-	(void)argc;
-	fd = open(argv[1], O_RDONLY);
-	i = 1;
-	while (i == 1)
-	{
-		i = get_next_line(fd, &line);
-		ft_strdel(&line);
-	}
-	close(fd);
-}*/
--- gnl/get_next_line.h
+++ gnl/get_next_line.h
@@ -6,22 +6,21 @@
 /*   By: loiberti <marvin@le-101.fr>                +:+   +:    +:    +:+     */
 /*                                                 #+#   #+    #+    #+#      */
 /*   Created: 2018/10/10 12:51:08 by loiberti     #+#   ##    ##    #+#       */
-/*   Updated: 2018/10/19 20:43:16 by loiberti    ###    #+. /#+    ###.fr     */
+/*   Updated: 2018/10/23 11:42:01 by loiberti    ###    #+. /#+    ###.fr     */
 /*                                                         /                  */
 /*                                                        /                   */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 
-#include <stdlib.h>
-#include <unistd.h>
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <sys/uio.h>
-#include <fcntl.h>
-
 # define GET_NEXT_LINE_H
-# define BUFF_SIZE 5
+
+# include <unistd.h>
+# include <sys/types.h>
+# include <sys/uio.h>
+# include "libft/includes/libft.h"
+
+# define BUFF_SIZE 1
 
 int		get_next_line(const int fd, char **line);
 
