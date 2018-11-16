ddec34b1072ab4e0623e36a6b862ae7285e81387
Loic Bertin <loiberti@z4r2p2.le-101.fr>
2018-11-16 22:57:22 +0100

    updates

--- printf/parse_string.c
+++ printf/parse_string.c
@@ -6,7 +6,7 @@
 /*   By: loiberti <marvin@le-101.fr>                +:+   +:    +:    +:+     */
 /*                                                 #+#   #+    #+    #+#      */
 /*   Created: 2018/11/15 17:54:28 by loiberti     #+#   ##    ##    #+#       */
-/*   Updated: 2018/11/16 22:33:08 by loiberti    ###    #+. /#+    ###.fr     */
+/*   Updated: 2018/11/16 22:56:15 by loiberti    ###    #+. /#+    ###.fr     */
 /*                                                         /                  */
 /*                                                        /                   */
 /* ************************************************************************** */
@@ -16,12 +16,21 @@
 int	stock_ordinary_char(char **str, char *format, int *i)
 {
 	int	k;
+	int	count;
 
 	k = 0;
+	count = 0;
 	if (format[*i] == '%' && format[*i + 1] != '%')
+	{
+		*i += 1;
 		return (0);
+	}
 	while (format[*i])
 	{
+		if (format[*i] == '%')
+			count++;
+		if (count == 3)
+			break;
 		if (format[*i] == '%' && format[*i + 1] != '%' && format[*i - 1] != '%')
 		{
 			*i += 1;
@@ -127,13 +136,17 @@ void	stock_conversion_indicator(e_ci *conversion_indicator, char *format, int *i
 		*conversion_indicator = f;
 }
 
-void	stock_arg_description(t_arg *param, char *format, int *i)
+int	stock_arg_description(t_arg *param, char *format, int *i)
 {
 	//stock_flag(param->flag, format, i);
 	//stock_field(&param->field, format, i);
 	//stock_precision(&param->precision, format, i);
 	//stock_length_modifier(&param->length_modifier, format, i);
 	stock_conversion_indicator(&param->conversion_indicator, format, i);
+	if (param->conversion_indicator == woaw)
+		return (1);
+	return (0);
+
 }
 
 
@@ -157,9 +170,12 @@ t_arg	*parse_string(const char *format)
 		if (format[i])
 		{
 			stock_arg_description(param, (char*)format, &i);
-			param->next = create_elem();
-			param = param->next;
-			i++;
+			if (param->conversion_indicator != woaw)
+			{
+				param->next = create_elem();
+				param = param->next;
+				i++;
+			}
 		}
 	}
 	return (begin_list);
@@ -169,7 +185,7 @@ int main()
 {
 	t_arg *test;
 
-	test = parse_string("bonjour%dcomment %dva la famille ?");
+	test = parse_string("%d%%%d%%%%%d%%%d");
 	display_list_content(test);
 	return (0);
 }
