9f6ba2dc99fc2921d06ba2de9d71e27d9dd9192a
Loic Bertin <loiberti@z1r4p3.le-101.fr>
2018-11-25 08:35:42 +0100

    add binary converter

--- printf/test.c
+++ printf/test.c
@@ -5,60 +5,38 @@
 /*                                                 +:+:+   +:    +:  +:+:+    */
 /*   By: loiberti <marvin@le-101.fr>                +:+   +:    +:    +:+     */
 /*                                                 #+#   #+    #+    #+#      */
-/*   Created: 2018/11/08 13:14:08 by loiberti     #+#   ##    ##    #+#       */
-/*   Updated: 2018/11/20 12:43:59 by loiberti    ###    #+. /#+    ###.fr     */
+/*   Created: 2018/11/25 05:16:18 by loiberti     #+#   ##    ##    #+#       */
+/*   Updated: 2018/11/25 08:32:25 by loiberti    ###    #+. /#+    ###.fr     */
 /*                                                         /                  */
 /*                                                        /                   */
 /* ************************************************************************** */
 
-#include <wchar.h>
-#include <locale.h>
-#include <stdarg.h>
-#include <string.h>
-#include <unistd.h>
-#include <stdlib.h>
+#include "printf.h"
 
-/*int ft_printf(const char *format-string, ...)
-  {
-  va_list va;
-  va_start (va, n);
-  int i = 0;
-
-  while (i < ap)
-  {
-  int x = va_arg (va, int);
-
-  }
-
-  printf("%c ", x);
-  printf("%s ", z);
-  printf("%d", y);
-
-  putchar ('\n');
-  va_end (va);
-  }*/
-
-
-int main (void)
+char *binary_converter(double nb)
 {
-	wchar_t *s;
-	int *t;
+	unsigned long long i;
+	char *str;
+	int j;
 
-	s = (wchar_t *) malloc(sizeof(wchar_t) * 2);
-	s[0] = 0x80;
-	s[1] = 0xC2;
-	t = (int*) malloc(sizeof(*t) * 2);
-	t[0] = 206;
-	t[1] = 177;
-	write(1, &(t[0]), 1);
-	write(1, &(t[1]), 1);
-
-	setlocale(LC_ALL, "");
-
-	printf("%C\n", 945);
-	putwchar(945);
-
-	return (0);
+	i = *(unsigned long long *)&nb;
+	j = 0;
+	str = ft_strnew(65);
+	while (j <= 63)
+	{
+		str[j] =  ((i>>j)&1) + '0';
+		j++;
+	}
+	str[64] = '\0';
+	return (str);
 }
 
+int main()
+{
+	char *str;
 
+	str = ft_strrev(binary_converter(-123.8));
+	printf("%s\n", str);
+	printf("%d", ft_strlen(str));
+	return (0);
+}
