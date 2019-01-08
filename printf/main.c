8afc529a3621a0224e3d84caa8597e2110f9d36b
Loic Bertin <loiberti@z4r3p3.le-101.fr>
2019-01-08 19:22:19 +0100

    add main

--- printf/main.c
+++ printf/main.c
@@ -6,7 +6,7 @@
 /*   By: loiberti <marvin@le-101.fr>                +:+   +:    +:    +:+     */
 /*                                                 #+#   #+    #+    #+#      */
 /*   Created: 2019/01/08 17:06:26 by loiberti     #+#   ##    ##    #+#       */
-/*   Updated: 2019/01/08 17:06:30 by loiberti    ###    #+. /#+    ###.fr     */
+/*   Updated: 2019/01/08 19:22:04 by loiberti    ###    #+. /#+    ###.fr     */
 /*                                                         /                  */
 /*                                                        /                   */
 /* ************************************************************************** */
@@ -27,8 +27,12 @@ int	main()
 	print = printf("%u %x %X %c %hhd %lld %.30lf %o %#o\n", -42, 42, 42, 'c', 128, 9000, 3.1400000000004200000001, -486, -486);
 	printf("%d %d\n\n", my_print, print);
 
-	my_print = printf("%p %hhd %lld %d %ld\n", "test", 128, 9000, -2147483649, -2147483649);
-	print = ft_printf("%p %hhd %lld %d %ld\n", "test", 128, 9000, -2147483649, -2147483649);
+	my_print = printf("%p %hhd %lld %d %ld %hd\n", "test", 128, 9000, -2147483649, -2147483649, 1000);
+	print = ft_printf("%p %hhd %lld %d %ld %hd\n", "test", 128, 9000, -2147483649, -2147483649, 1000);
+	printf("%i %i\n\n", my_print, print);
+
+	my_print = printf("%hhu %llu %u %lu %hu\n", -128, -9000, -2147483649, -2147483649, -1000);
+	print = ft_printf("%hhu %llu %u %lu %hu\n", -128, -9000, -2147483649, -2147483649, -1000);
 	printf("%i %i\n\n", my_print, print);
 
 	my_print = ft_printf("%+d %+d % d %15d %-15d %015d %+015d\n", 42, -42, 42, 42, 42, 42, -42);
@@ -39,8 +43,8 @@ int	main()
 	print = printf("%x %#x %#015X %p %.p\n", 12, 12, 12, NULL, NULL);
 	printf("%i %i\n\n", my_print, print);
 
-	my_print = ft_printf("%.17f %f %.f %.50f %.1f %.1f %#.f\n", 47853154.123456, 4.895, 4.895, 4.895, 1399.999999, 1399.89999, 42.89);
-	print = printf("%.17f %f %.f %.50f %.1f %.1f %#.f\n", 47853154.123456, 4.895, 4.895, 4.895, 1399.999999, 1399.89999, 42.89);
+	my_print = ft_printf("%.17f %f %.f %.50f %.1f %.1f %#.f %Lf %lf\n", 47853154.123456, 4.895, 4.895, 4.895, 1399.999999, 1399.89999, 42.89, (long double)888888888888888888.8789789, 987.789);
+	print = printf("%.17f %f %.f %.50f %.1f %.1f %#.f %Lf %lf\n", 47853154.123456, 4.895, 4.895, 4.895, 1399.999999, 1399.89999, 42.89, (long double)888888888888888888.8789789, 987.789);
 	printf("%d %d\n\n", my_print, print);
 
 	my_print = ft_printf("%.200f\n", 42.895);
@@ -51,9 +55,9 @@ int	main()
 	print = printf("%+f %+f %+015f\n", 42.42, -42.42, -42.456456456);
 	printf("%d %d\n\n", my_print, print);
 
-	str = ft_sprintf("%u %x %X %c %hhd %lld %.30lf %o %#o\n", -42, 42, 42, 'c', 128, 9000, 3.1400000000004200000001, -486, -486);
+	str = ft_sprintf("%u %x %X %c %hhd %lld %.30lf %o %#o -abcdef\n", -42, 42, 42, 'c', 128, 9000, 3.1400000000004200000001, -486, -486);
 	my_print = printf("%s", str);
-	print = printf("%u %x %X %c %hhd %lld %.30lf %o %#o\n", -42, 42, 42, 'c', 128, 9000, 3.1400000000004200000001, -486, -486);
+	print = printf("%u %x %X %c %hhd %lld %.30lf %o %#o -abcdef\n", -42, 42, 42, 'c', 128, 9000, 3.1400000000004200000001, -486, -486);
 	printf("%d %d\n\n", my_print, print);
 	free(str);
 }
