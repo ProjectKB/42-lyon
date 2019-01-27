b462a1d1608d018c211d4f3e71bc9e5ca8af8ed2
Loic Bertin <loiberti@z4r1p4.le-101.fr>
2019-01-27 10:39:36 +0100

    fonctions ok

--- fdf/tools.c
+++ fdf/tools.c
@@ -28,7 +28,7 @@ t_param *init_param(int width, int height)
     param->z_obl = 1.0;
     param->move_w = 0;
     param->move_h = 0;
-    param->algo = 0;
+    param->algo = 1;
     param->rot->angle = 0.0;
     param->rot->mod = 2;
     return (param);
