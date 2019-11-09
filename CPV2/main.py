9054076d747fd8b7fbe5045d394f906eb982dd1f
Loic Bertin <loiberti@z4r3p3.le-101.fr>
2019-11-09 20:45:36 +0100

    add detection and parsing of matrices

--- CPV2/main.py
+++ CPV2/main.py
@@ -6,7 +6,7 @@
 #    By: loiberti <marvin@le-101.fr>                +:+   +:    +:    +:+      #
 #                                                  #+#   #+    #+    #+#       #
 #    Created: 2019/11/08 21:20:56 by loiberti     #+#   ##    ##    #+#        #
-#    Updated: 2019/11/08 22:44:45 by loiberti    ###    #+. /#+    ###.fr      #
+#    Updated: 2019/11/09 20:43:46 by loiberti    ###    #+. /#+    ###.fr      #
 #                                                          /                   #
 #                                                         /                    #
 # **************************************************************************** #
@@ -14,29 +14,44 @@
 import sys
 import re
 
-def assignation():
-    print("This is an assignation.")
+REEL = 0
+IMAGINAIRE = 1
+MATRICE = 2
+POLY = 3
 
-def calcul():
-    print("This is a basic calcul")
+def parse_matrices(args):
+        matrices = []
+        args = re.findall(r'\[((\[(,?-?\d+(.\d+)?)+\];?)+)\]', args)
+        for arg in args:
+            lines = arg[0].split(';')
+            matrix = []
+            for elem in lines:
+                tmp = elem.replace('[', '')
+                tmp = tmp.replace(']', '')
+                line = tmp.split(',')
+                matrix.append(line)
+            matrices.append(matrix)
+        return matrices
 
-def main():
-    print("")
+def find_calcul_type(args):
+    if re.search(r'\[((\[(,?-?\d+(.\d+)?)+\];?)+)\]', args):
+        matrices = parse_matrices(args)
+        print(matrices)
+        print("There are some matrices in this expression.")
+
+
+def processing_loop():
     while 42:
         try:
-            args = input("-> ").replace(' ', '')
+            arg = input("\n-> ").replace(' ', '')
         except (EOFError, KeyboardInterrupt):
-            sys.exit("\nYou're such a rude...\n")
-        if args == "quit":
+            sys.exit("\nA true gentleman would have written \"quit\", you're such a rude...\n")
+        if arg == "quit":
             sys.exit("Bye Honey !\n")
-        c_assign = re.search(r'^(var\w+=|fun\w+\(x\)=)', args)
-        if c_assign != None:
-            c_assign = c_assign.group(0).replace('=', '')
-            assignation()
-        else:
-            calcul()
-        print(c_assign, args + "\n")
+        find_calcul_type(arg)
 
+def main():
+    processing_loop()
 
 if __name__ == "__main__":
     main()
