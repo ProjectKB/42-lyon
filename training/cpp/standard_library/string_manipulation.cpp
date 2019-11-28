a1098138e1455101754aba23881e1311e0e1a386
Loic Bertin <loiberti@z4r4p2.le-101.fr>
2019-11-28 19:25:24 +0100

    string manipulation

--- training/cpp/standard_library/string_manipulation.cpp
+++ training/cpp/standard_library/string_manipulation.cpp
@@ -6,7 +6,7 @@
 /*   By: loiberti <marvin@le-101.fr>                +:+   +:    +:    +:+     */
 /*                                                 #+#   #+    #+    #+#      */
 /*   Created: 2019/11/28 16:44:37 by loiberti     #+#   ##    ##    #+#       */
-/*   Updated: 2019/11/28 16:58:13 by loiberti    ###    #+. /#+    ###.fr     */
+/*   Updated: 2019/11/28 19:22:20 by loiberti    ###    #+. /#+    ###.fr     */
 /*                                                         /                  */
 /*                                                        /                   */
 /* ************************************************************************** */
@@ -18,22 +18,21 @@
 using namespace std;
 typedef string str;
 
-struct IsVowel
+struct	TestVowel
 {
-	bool operator()(const str &s) const
+	bool operator()(unsigned char c)
 	{
-		for (size_t i(0); i < s.size(); ++i)
-			switch (s[i])
-			{
-				case 'a':
-				case 'e':
-				case 'i':
-				case 'o':
-				case 'u':
-				case 'y':
-					return true;
-				default:
-					break;
+		switch (c)
+		{
+			case 'a':
+			case 'e':
+			case 'i':
+			case 'o':
+			case 'u':
+			case 'y':
+				return true;
+			default:
+				break;
 			}
 		return false;
 	}
@@ -42,8 +41,25 @@ struct IsVowel
 int main()
 {
 	str	s1;
+	str	s2;
+	TestVowel f;
+	back_insert_iterator<str> it(s2);
 
 	s1 = "Bien le bonjour";
-	remove_if(s1.begin(), s1.end(), IsVowel(s1));
+
+	s1.erase(remove(s1.begin(), s1.end(), ' '), s1.end());
 	cout << s1 << "\n";
+
+	s1.erase(s1.begin(), s1.begin() + 2);
+	cout << s1 << "\n";
+
+	reverse(s1.begin(), s1.end());
+	cout << s1 << "\n";
+
+	for (str::iterator it = s1.begin(); it != s1.end(); ++it)
+		if (!(f(*it)))
+			s2.push_back(*it);
+	cout << s2 << "\n";
+
+	return (0);
 }
