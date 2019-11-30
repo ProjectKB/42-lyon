37023f3aea1ed738980d2525187e5b61584ab9fa
Loic Bertin <loiberti@z4r4p3.le-101.fr>
2019-11-30 15:15:40 +0100

    string manipulation + tricks -> algo/functor

--- training/cpp/standard_library/string_manipulation.cpp
+++ training/cpp/standard_library/string_manipulation.cpp
@@ -6,7 +6,7 @@
 /*   By: loiberti <marvin@le-101.fr>                +:+   +:    +:    +:+     */
 /*                                                 #+#   #+    #+    #+#      */
 /*   Created: 2019/11/28 16:44:37 by loiberti     #+#   ##    ##    #+#       */
-/*   Updated: 2019/11/28 19:22:20 by loiberti    ###    #+. /#+    ###.fr     */
+/*   Updated: 2019/11/30 14:29:47 by loiberti    ###    #+. /#+    ###.fr     */
 /*                                                         /                  */
 /*                                                        /                   */
 /* ************************************************************************** */
@@ -14,6 +14,9 @@
 #include <string>
 #include <iostream>
 #include <algorithm>
+#include <iterator>
+#include <cctype>
+#include <functional>
 
 using namespace std;
 typedef string str;
@@ -38,28 +41,88 @@ struct	TestVowel
 	}
 };
 
+template <typename T>
+void	display_tab(T tab)
+{
+	copy(tab.begin(), tab.end(), ostream_iterator<char>(cout, ""));
+	cout << "\n";
+}
+
+struct To_Upper
+{
+	void	operator()(char &c)
+	{
+		c = toupper(c);
+	}
+};
+
+void	to_upper(char &c)
+{
+	c = toupper(c);
+}
+
+char	to_upper2(const char &c)
+{
+	return toupper(c);
+}
+
+bool	is_palind(str palind)
+{
+	back_insert_iterator<str>	it(palind);
+
+	for_each(palind.begin(), palind.end(), to_upper);
+	transform(palind.begin(), palind.end(), palind.begin(), to_upper2);
+	cout << palind;
+	if (*(mismatch(palind.begin(), palind.end(), palind.rbegin()).first))
+		return false;
+	return true;
+}
+
 int main()
 {
 	str	s1;
 	str	s2;
-	TestVowel f;
-	back_insert_iterator<str> it(s2);
+	TestVowel	f;
+	back_insert_iterator<str>	it(s2);
+	str::iterator	it2;
+	str	haystack;
+	str	needle;
+	str	palind;
 
 	s1 = "Bien le bonjour";
+	haystack = "La bonne journee, sir.";
+	needle = "bonne journee";
+	str::iterator n;
 
+	// remove x
 	s1.erase(remove(s1.begin(), s1.end(), ' '), s1.end());
-	cout << s1 << "\n";
+	display_tab(s1);
 
+	// remove with range
 	s1.erase(s1.begin(), s1.begin() + 2);
-	cout << s1 << "\n";
+	display_tab(s1);
 
+	// reverse x
 	reverse(s1.begin(), s1.end());
-	cout << s1 << "\n";
+	display_tab(s1);
 
+	// apply functor for create new x
 	for (str::iterator it = s1.begin(); it != s1.end(); ++it)
 		if (!(f(*it)))
 			s2.push_back(*it);
-	cout << s2 << "\n";
+	display_tab(s2);
+
+	// find elem
+	it2 = find(s2.begin(), s2.end(), 'j');
+	it2 != s2.end() ? cout << *it2 << "\n" : cout << "Element not found.\n";
+
+	// find subsequence into x
+	n = search(haystack.begin(), haystack.end(), needle.begin(), needle.end());
+	n != haystack.end() ? cout << "Item found at index " << n-haystack.begin() << "\n":
+		cout << "Element not foun.\n";
+
+	// test if palind us palindrome
+	cout << is_palind("aCCA") << "\n";
 
 	return (0);
 }
---
+++ training/cpp/standard_library/tricks.cpp
@@ -0,0 +1,103 @@
+/* ************************************************************************** */
+/*                                                          LE - /            */
+/*                                                              /             */
+/*   tricks.cpp                                       .::    .:/ .      .::   */
+/*                                                 +:+:+   +:    +:  +:+:+    */
+/*   By: loiberti <marvin@le-101.fr>                +:+   +:    +:    +:+     */
+/*                                                 #+#   #+    #+    #+#      */
+/*   Created: 2019/11/30 14:29:56 by loiberti     #+#   ##    ##    #+#       */
+/*   Updated: 2019/11/30 15:13:04 by loiberti    ###    #+. /#+    ###.fr     */
+/*                                                         /                  */
+/*                                                        /                   */
+/* ************************************************************************** */
+
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <iterator>
+
+using namespace std;
+
+typedef string str;
+
+void	display(str &s)
+{
+	copy(s.begin(), s.end(), ostream_iterator<char> (cout, ""));
+	cout << "\n";
+}
+
+void	to_upper(char &c)
+{
+	c = toupper(c);
+}
+
+char	to_upper2(const char &c)
+{
+	return toupper(c);
+}
+
+struct	To_Upper
+{
+	void	operator()(char &c) const
+	{
+		c = toupper(c);
+	}
+};
+
+struct	To_Upper2
+{
+	char	operator()(const char &c) const
+	{
+		return toupper(c);
+	}
+};
+
+int main()
+{
+	str s1("aCcA");
+	str s2("aCcA");
+	str s3("aCcA");
+	str s4("aCcA");
+	str s5("aCcA");
+	str s6("aCcA");
+	str s7("aCcA");
+	str s8("aCcA");
+	str s9("aCcA");
+	str s9c;
+	back_insert_iterator<str> it(s9c);
+	To_Upper f;
+	To_Upper2 f2;
+
+	for_each(s1.begin(), s1.end(), to_upper);
+	display(s1);
+
+	for_each(s3.begin(), s3.end(), To_Upper());
+	display(s3);
+
+	transform(s2.begin(), s2.end(), s2.begin(), to_upper2);
+	display(s2);
+
+	transform(s4.begin(), s4.end(), s4.begin(), To_Upper2());
+	display(s4);
+
+	for (str::iterator it(s5.begin()); it != s5.end(); ++it)
+		f(*it);
+	display(s5);
+
+	for (str::iterator it(s6.begin()); it != s6.end(); ++it)
+		*it = f2(*it);
+	display(s6);
+
+	for (size_t i(0); i < s7.size(); ++i)
+		to_upper(s7[i]);
+	display(s7);
+
+	for (size_t i(0); i < s8.size(); ++i)
+		s8[i] = to_upper2(s8[i]);
+	display(s8);
+
+	transform(s9.begin(), s9.end(), it, to_upper2);
+	display(s9);
+	display(s9c);
+	return (0);
+}
