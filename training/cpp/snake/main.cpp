2e41376f39dccbd687b55d03394f9023fdf4d144
Loic Bertin <loiberti@z4r4p5.le-101.fr>
2019-12-01 21:56:27 +0100

    prohibit turning back ok

--- training/cpp/snake/main.cpp
+++ training/cpp/snake/main.cpp
@@ -6,7 +6,7 @@
 /*   By: loiberti <marvin@le-101.fr>                +:+   +:    +:    +:+     */
 /*                                                 #+#   #+    #+    #+#      */
 /*   Created: 2019/11/30 17:57:08 by loiberti     #+#   ##    ##    #+#       */
-/*   Updated: 2019/12/01 21:14:03 by loiberti    ###    #+. /#+    ###.fr     */
+/*   Updated: 2019/12/01 21:54:46 by loiberti    ###    #+. /#+    ###.fr     */
 /*                                                         /                  */
 /*                                                        /                   */
 /* ************************************************************************** */
@@ -23,14 +23,11 @@ typedef string str;
 struct var
 {
 	WINDOW *win;
-	int		ch;
+	int		key;
 
-	int		h;
-	int		w;
-	int		ht;
-	int		wt;
-	int		tmp;
-	int		piece;
+	int		h_max;
+	int		w_max;
+	int		pieces;
 	deque<vector<int>>	pos;
 
 	void	init_screen()
@@ -46,24 +43,38 @@ struct var
 
 	void	init_var()
 	{
-		getmaxyx(stdscr, h, w);
-		win = newwin(h, w, 0, 0);
-		ht = h / 2;
-		piece = 1;
-		wt = w / 2;
-		pos.push_back({h / 2, w / 2});
+		getmaxyx(stdscr, h_max, w_max);
+		win = newwin(h_max, w_max, 0, 0);
+		pieces = 3;
+		pos.push_back({h_max / 2, w_max / 2});
+		pos.push_back({h_max / 2 - 1, w_max / 2});
+		pos.push_back({h_max / 2 - 2, w_max / 2});
 	}
 };
 
+bool prohibit_turning_back(int &o_key, int &n_key)
+{
+	if (o_key == KEY_UP && n_key == KEY_DOWN)
+		return (0);
+	else if (n_key == KEY_UP && o_key == KEY_DOWN)
+		return (0);
+	else if (n_key == KEY_LEFT && o_key == KEY_RIGHT)
+		return (0);
+	else if (o_key == KEY_LEFT && n_key == KEY_RIGHT)
+		return (0);
+	else if (n_key != KEY_UP && n_key != KEY_DOWN && n_key != KEY_LEFT && n_key != KEY_RIGHT)
+		return (0);
+	return (1);
+}
+
 void	manage_direction(var &v)
 {
-	int	l_key;
+	int	tmp_key;
 
-	l_key = v.tmp;
-	if ((v.ch = getch()) != ERR)
-		v.tmp = v.ch;
+	if ((tmp_key = getch()) != ERR && prohibit_turning_back(v.key, tmp_key))
+		v.key = tmp_key;
 	v.pos.push_front(v.pos[0]);
-	switch (v.tmp)
+	switch (v.key)
 	{
 		case KEY_UP:
 			--v.pos[0][0];
@@ -81,50 +92,53 @@ void	manage_direction(var &v)
 	v.pos.pop_back();
 }
 
-
-
 void	manage_border(var &v)
 {
-	if (v.pos[0][1] == v.w - 1)
+	if (v.pos[0][1] == v.w_max - 1)
 		v.pos[0][1] = 2;
 	else if (v.pos[0][1] == 1)
-		v.pos[0][1] = v.w - 2;
+		v.pos[0][1] = v.w_max - 2;
 	else if (v.pos[0][0] == 1)
-		v.pos[0][0] = v.h - 2;
-	else if (v.pos[0][0] == v.h - 1)
+		v.pos[0][0] = v.h_max - 2;
+	else if (v.pos[0][0] == v.h_max - 1)
 		v.pos[0][0] = 2;
 }
 
 void	add_piece(var &v)
 {
-	switch (v.tmp)
+	switch (v.key)
 	{
 		case KEY_UP:
-			v.pos.push_back({v.pos[v.piece - 1][0] - 1, v.pos[v.piece - 1][1]});
+			v.pos.push_back({v.pos[v.pieces - 1][0] - 1, v.pos[v.pieces - 1][1]});
 			break;
 		case KEY_DOWN:
-			v.pos.push_back({v.pos[v.piece - 1][0] + 1, v.pos[v.piece - 1][1]});
+			v.pos.push_back({v.pos[v.pieces - 1][0] + 1, v.pos[v.pieces - 1][1]});
 			break;
 		case KEY_LEFT:
-			v.pos.push_back({v.pos[v.piece - 1][0], v.pos[v.piece - 1][1] - 1});
+			v.pos.push_back({v.pos[v.pieces - 1][0], v.pos[v.pieces - 1][1] - 1});
 			break;
 		case KEY_RIGHT:
-			++v.pos[0][1];
-			v.pos.push_back({v.pos[v.piece - 1][0], v.pos[v.piece - 1][1] + 1});
+			v.pos.push_back({v.pos[v.pieces - 1][0], v.pos[v.pieces - 1][1] + 1});
 			break;
 	}
-	v.piece++;
+	++v.pieces;
+}
+
+void	display_pieces_data(var &v)
+{
+	for (int i(0); i < v.pieces; i++)
+		cout << "h: " << v.pos[i][0] << " | w: " << v.pos[i][1] << "\n";
 }
 
 void	display_pieces(var &v)
 {
-	for (int i(0); i < v.piece; i++)
-		cout << "h: " << v.pos[i][0] << " | w: " << v.pos[i][1] << "\n";
+	for (int i(0); i < v.pieces - 1; ++i)
+		mvwaddch(v.win, v.pos[i][0], v.pos[i][1], ACS_CKBOARD);
 }
 
 void	erase_piece(var &v)
 {
-		mvwaddch(v.win, v.pos[v.piece - 1][0], v.pos[v.piece - 1][1], ' ');
+	mvwaddch(v.win, v.pos[v.pieces - 1][0], v.pos[v.pieces - 1][1], ' ');
 }
 
 int main()
@@ -133,19 +147,13 @@ int main()
 
 	v.init_screen();
 	v.init_var();
-	v.tmp = KEY_UP;
-	add_piece(v);
-	add_piece(v);
-	//display_pieces(v);
 	while (1)
 	{
 		erase_piece(v);
 		manage_direction(v);
 		manage_border(v);
-		mvwaddch(v.win, v.pos[0][0], v.pos[0][1], ACS_CKBOARD);
-		mvwaddch(v.win, v.pos[1][0], v.pos[1][1], ACS_CKBOARD);
-		mvwaddch(v.win, v.pos[2][0], v.pos[2][1], ACS_CKBOARD);
-		(v.tmp == KEY_UP || v.tmp == KEY_DOWN) ? usleep(80000) : usleep(40000);
+		display_pieces(v);
+		(v.key == KEY_UP || v.key == KEY_DOWN) ? usleep(80000) : usleep(40000);
 		wrefresh(v.win);
 	}
 	endwin();
