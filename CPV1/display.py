5bcf6bd0082a694465308f9ae772039d302f1a85
Loïc Bertin <loic@MacBook-Pro-de-Loic.local>
2020-09-10 17:00:23 +0200

    add new test

--- CPV1/display.py
+++ CPV1/display.py
@@ -35,8 +35,8 @@ def second_degree(delta, a, b, c, resultat, x1, x2):
         print("\tDelta is smaller than 0, there are two solutions to the equation.\n" + \
             "\tResolution formula: x\u2081, x\u2082 = (-b ± i\u221A|\u0394|) / 2a\n\n" + \
             "\tSolutions are:\n" + \
-            "\tx\u2081 = (" + str(b * -1) + " - i\u221A" + str(-delta) + ") / " +  str(2 * a) + "\n" + \
-            "\tx\u2082 = (" + str(b * -1) + " + i\u221A" + str(-delta) + ") / " + str(2 * a) + "\n")
+            "\tx\u2081 = (" + str(b * -1) + " - " + str(tools.roundornot(tools.newton_sqrt(-delta))) + "i) / " +  str(2 * a) + "\n" + \
+            "\tx\u2082 = (" + str(b * -1) + " + " + str(tools.roundornot(tools.newton_sqrt(-delta))) + "i) / " + str(2 * a) + "\n")
     elif delta == 0:
         print("\tDelta is equal to 0, there is one solution to the equation.\n\t" + \
             "Resolution formula: -(b / 2a)\n\n" + \
